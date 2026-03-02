314dbc0984d7e7afaf3bc9a8506e22a57547a1df
ImManiKanta <chinthamanikanta1@gmail.com>
2026-03-02 12:43:41 +0530

    terraform tfvars

---
+++ terraform-multi-env/tfvars/dev/backend.tf
@@ -0,0 +1,5 @@
+bucket = "tf-remotestate-mani-dev"
+key    = "terraform.tfstate"
+region = "us-east-1"
+use_lockfile = true # This is the default behavior now
+encrypt        = true
---
+++ terraform-multi-env/tfvars/prod/backend.tf
@@ -0,0 +1,5 @@
+bucket       = "tf-remotestate-mani-prod"
+key          = "terraform.tfstate"
+region       = "us-east-1"
+use_lockfile = true # This is the default behavior now
+encrypt      = true
